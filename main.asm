5431fc25ff0e2c63d6958f8a06e2141fff1fcf21
Can Deniz <dcb.denizcan@gmail.com>
2021-04-18 17:56:41 +0300

    will be linkable with C

--- main.asm
+++ main.asm
@@ -1,10 +1,5 @@
-;; r16 r17 -> argument to functions
-;; r18, r19 -> return from functions
-;; r20, ..., r25 -> using in interrupt handlers
-;; r16, r17, r18, r19 could be manipulated at return a function
-
-.equ HALF_TIME = 0x7f
-.equ FULL_TIME = 0xff
+.equ HALF_TIME = 0x2f
+.equ FULL_TIME = 0x7f
 
 .cseg
 .org 0x00
@@ -41,177 +36,199 @@ jmp timer0_compb_handler ; timer0 compb
 reti
 nop					; timer0 ovf
 
-
-
-
-
 int0_handler:
-	eor r21, r21
-	out TCNT0, r21
-	ldi r20, HALF_TIME
-	out OCR0B, r20
-	ldi r20, 0x04
-	ldi ZH, high(TIMSK0)
-	ldi ZL, low(TIMSK0)
-	st Z, r20				; set timer0 compb module with half time
-	ldi r20, 0xff
-	out OCR0A, r20
+	push r16
 	
-	out EIMSK, r21			; disable int0
+	ldi r16, HALF_TIME 
+	call set_timer_b			; set timer
 	
-	ldi ZH, high(data)
-	ldi ZL, low(data)
-	st Z, r23				; data to 0
+	ldi XL, low(control)
+	ldi XH, high(control)
+	ldi r20, 0x03
+	st X, r20					; serial flag and half flag are set
 	
-	ldi ZH, high(control)
-	ldi ZL, low(control)
-	ldi r21, 0x01
-	st Z, r21				; set half flag in control register
+	eor r20, r20
+	out EIMSK, r20				; disable int0
+	
+	ldi XH, high(data)
+	ldi XL, low(data)
+	st X, r20					; data to 0x00
+	
+	pop r16
 	reti
-
-timer0_compb_handler:
-	ldi ZH, high(control)
-	ldi ZL, low(control)
-	ld r20, Z
-	mov r21, r20
-	andi r21, 0x01
-	sbrc r21, 0x00		; if half flag is cleared, full_handler runs
-	rjmp half_handler	; else rjmp .half_handler
-						; sbrc -> skip if bit in register is cleared
 	
-	full_handler:
-		ldi YH, high(counter)
-		ldi YL, low(counter)
-		ld r20, Y
-		cpi r20, 0x08
-		breq exit_full_handler
-		continue_full_handler:
-			ldi ZH, high(data)
-			ldi ZL, low(data)
-			ld r23, Z
-			lsr r23
-			in r22, PINC
-			lsr r22
-			sbrc r22, 0x00
-			ori r23, 0x80
-			st Z, r23
-			inc r20
-			st Y, r20
-			rjmp end_of_full_handler
-		exit_full_handler:
-			call handler_exit
-		end_of_full_handler:
-			rjmp timer0_compb_handler_end
 	
-	half_handler:
-		in r21, PINC
-		lsr r21
-		andi r21, 0x01
-		sbrc r21, 0x00
-		rjmp false_alarm
-		true_alarm:
-			ldi YH, high(counter)
-			ldi YL, low(counter)
-			ld r23, Y
-			inc r23
-			st Y, r23
-			andi r20, 0xfe
-			st Z, r20			; half flag to zero
-			ldi r20, FULL_TIME
-			out OCR0B, r20		; set compare b module with full time
-			rjmp timer0_compb_handler_end
-		false_alarm:
-			call handler_exit
-
-	timer0_compb_handler_end:
-		reti
+	
 	
 timer0_compa_handler:
-	ldi YH, high(counter)
-	ldi YL, low(counter)
-	ld r20, Y
+	ldi XH, high(counter)
+	ldi XL, low(counter)
+	ld r20, X
 	cpi r20, 0x08
-	breq exit_compa_handler
-	continue_compa_handler:
+	breq exit_handler_a			; if counter==8 exit
+	
+	inc r20
+	st X, r20					; ++counter
+	
+	ldi XH, high(data)
+	ldi XL, low(data)
+	ld r20, X					; get data
+	
+	mov r21, r20
+	andi r21, 0x01
+	out PORTC, r21				; tx 1 or 0 others 0
+	
+	lsr r20
+	st X, r20
+	reti						; shift data to right and return
+	
+	exit_handler_a:
+	call handler_exit
+	reti
+	
+	
+	
+	
+timer0_compb_handler:
+	ldi XH, high(control)
+	ldi XL, low(control)
+	ld r20, X
+	sbrc r20, 0x00		
+	rjmp half_handler			; if half flag is set, jump to half_handler
+					
+	full_handler:
+		ldi XH, high(counter)
+		ldi XL, low(counter)
+		ld r20, X
+		cpi r20, 0x08			
+		breq exit_handler_b		; if counter==8 exit
+		
 		inc r20
-		st Y, r20
-		ldi YH, high(data)
-		ldi YL, low(data)
-		ld r20, Y
-		mov r21, r20
-		andi r21, 0x01
-		cpi r21, 0x0
-		breq clr_tx
-		set_tx:
-			ldi r22,0x01
-			rjmp set_clr_tx_end
-		clr_tx:
-			ldi r22, 0x0
-		set_clr_tx_end:
-			out PORTC, r22
-			lsr r20
-			st Y, r20
-			rjmp end_of_timer0_compa_handler
-	
-	exit_compa_handler:
+		st X, r20				; ++counter
+		
+		ldi XH, high(data)
+		ldi XL, low(data)
+		ld r20, X
+		lsr r20					; data >>= 1
+		
+		in r21, PINC
+		sbrc r21, 0x01
+		ori r20, 0x80
+		st X, r20				; if rx==1 data|=0b10000000
+		reti
+		
+		exit_handler_b:
 		call handler_exit
+		reti
 	
-	end_of_timer0_compa_handler:
+	half_handler:
+		push r16
+		ldi r16, FULL_TIME
+		call set_timer_b			; set compare b module with full time
+		pop r16
+		
+		in r21, PINC
+		lsr r21
+		sbrc r21, 0x00
+		rjmp false_alarm			; if half flag is set jump false_alarm
+		
+		true_alarm:
+		ldi XH, high(counter)
+		ldi XL, low(counter)
+		ld r20, X
+		inc r20
+		st X, r20					; ++counter
+		
+		ldi XH, high(control)
+		ldi XL, low(control)
+		ldi r20, 0x02
+		st X, r20					; half flag to zero
+		reti
+		
+		false_alarm:
+		call handler_exit
 		reti
 
 	
+
+	
+	
 handler_exit:
-	eor r23,r23
-	ldi ZH, high(TIMSK0)
-	ldi ZL, low(TIMSK0)
-	st Z, r23				; turn off timer interrupts
+	call stop_timers
 	
-	ldi ZH, high(counter)
-	ldi ZL, low(counter)
-	st Z, r23				; counter to 0
+	eor r20,r20
+	ldi XH, high(counter)
+	ldi XL, low(counter)
+	st X, r20					; counter to 0
 	
-	ldi ZH, high(control)
-	ldi ZL, low(control)
-	st Z, r23				; control to 0
+	ldi XH, high(control)
+	ldi XL, low(control)
+	st X, r20					; control to 0
 	
-	ldi r24, 0x01
-	out EIMSK, r24			; turn on int0
+	ldi r20, 0x01
+	out EIMSK, r20				; turn on int0
 	
-	out PORTC, r24
+	out PORTC, r20				; tx will be set
+	
+	ldi XH, high(data)
+	ldi XL, low(data)
+	ld r20, X
+	ldi XH, high(recv_data)
+	ldi XL, low(recv_data)
+	st X, r20
 	ret
 	
 	
-
+	
+	
+	
 start:
-	call set_timer_settings
-	call set_port_settings
-	call set_int0_settings
-	sei
+	call init_serial
 	
-	;ldi r16, 0xac
-	;call send
+	
+	
+	program:
+	; sender side
+	; ldi r16, 0xaa
+	; call send
+	
+	; receiver side
+	call recv
+	cpi r16, 0xaa
+	breq call_test
+	ldi r16, 0xff
+	call test
+	rjmp program
+	call_test:
+	ldi r16, 0x00
+	call test
+	rjmp program
 
-end_of_program:
-	rjmp end_of_program
+
+init_serial:
+	call init_timers
+	call init_ports
+	call init_int0
+	sei
+	ret
 	
 	
-	
-set_timer_settings:	
+init_timers:
 	ldi r16, 0x02
 	out TCCR0A, r16
-	ldi r16, 0x01
-	out TCCR0B, r16
+	ldi r17, 0x01
+	out TCCR0B, r17
 	ret
 	
-set_port_settings:
-	eor r16, r16
+init_ports:
+	eor r16,r16
 	out DDRD, r16
 	ldi r16, 0x01
 	out DDRC, r16
 	out PORTC, r16
 	ret
 	
-set_int0_settings:
+init_int0:
 	ldi r16, 0x02
 	ldi XH, high(EICRA)
 	ldi XL, low(EICRA)
@@ -220,18 +237,51 @@ set_int0_settings:
 	out EIMSK, r16
 	ret
 	
+set_timer_a:
+	eor r17,r17
+	out TCNT0, r17			; timer count is 0
+
+	out OCR0A, r16			; set timer's length
 	
-write_data:
-	ldi XH, high(data)
-	ldi XL, low(data)
-	st X, r16
+	ldi r17, 0x06
+	out TIFR0, r17			; clear interrupt flags
+	
+	ldi r17, 0x02
+	ldi XH, high(TIMSK0)
+	ldi XL, low(TIMSK0)
+	st X, r17				; activate timer compare interrupts
+	
+	ret
+	
+	
+set_timer_b:
+	eor r20,r20
+	out TCNT0, r20			; timer count is 0
+
+	out OCR0A, r16
+	out OCR0B, r16			; set timer's length
+	
+	ldi r20, 0x06
+	out TIFR0, r20			; clear interrupt flags
+	
+	ldi r20, 0x04
+	ldi XH, high(TIMSK0)
+	ldi XL, low(TIMSK0)
+	st X, r20				; activate timer compare interrupts
+	
+	ret
+	
+	
+	
+stop_timers:
+	ldi r20, 0x00
+	ldi XH, high(TIMSK0)
+	ldi XL, low(TIMSK0)
+	st Z, r20				; turn off timer compare interrupts
 	ret
 
-read_data:
-	ldi XH, high(data)
-	ldi XL, low(data)
-	ld r18, X
-	ret
+	
+
 	
 test:
 	ldi r17, 0xff
@@ -239,24 +289,55 @@ test:
 	out PORTB, r16
 	ret
 	
+	
 send:
-	call write_data
+	ldi ZH, high(control)
+	ldi ZL, low(control)
 	
-	eor r18, r18
-	out TCNT0, r18
-	ldi r19, FULL_TIME
-	out OCR0A, r19
-	ldi r19, 0x02
-	ldi ZH, high(TIMSK0)
-	ldi ZL, low(TIMSK0)
-	st Z, r19				; set timer0 compb module with half time
-	ldi r19, 0xff
-	out OCR0B, r19
+	wait_for_send:
+	ld r17, Z
+	sbrc r17, 0x2
+	rjmp wait_for_send		; wait to end serial
 	
-	out EIMSK, r18			; disable int0
+	eor r17, r17
+	out EIMSK, r17			; disable int0
+	
+	ldi r17, 0x06
+	st Z, r17				; serial and send flag is set
+	
+	ldi ZH, high(data)
+	ldi ZL, low(data)
+	st Z, r16				; write data
+	
+	ldi r16, FULL_TIME
+	call set_timer_a		; set_timer
+	
+	ret	
+	
+	
+recv:
+	ldi ZH, high(control)
+	ldi ZL, low(control)
+	
+	wait_for_recv:
+	ld r16, Z
+	sbrc r16, 0x2
+	rjmp wait_for_recv		; wait to end serial
+	
+	ldi ZH, high(recv_data)
+	ldi ZL, low(recv_data)
+	ld r16, Z
+	ldi r17, 0xff
+	st Z, r17
+	
 	ret
 	
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; control: - - - - - send serial half ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .dseg
 	data: .db 0x00
-	control: .db 0x00
 	counter: .db 0x00
+	control: .db 0x00
+	recv_data: .db 0x00
